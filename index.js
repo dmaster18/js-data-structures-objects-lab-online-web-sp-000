491d3fabf97b4966460ccca8be082aac17e30351
David Strauch <dmaster18@gmail.com>
2020-08-16 21:29:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 
 function deleteFromDriverByKey(driver, key) {
   let newDriver = driver;
-  delete newDriver.key;
+  delete newDriver[key];
   return newDriver;
 
 }
